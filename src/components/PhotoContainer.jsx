f088f8b1bfbcfb8368b22a15649b37b6ed4055cd
kenju <ken901waga@gmail.com>
2019-04-20 16:16:59 +0900

    fix typo

--- src/components/PhotoContainer.jsx
+++ src/components/PhotoContainer.jsx
@@ -1,19 +1,15 @@
 import React from "react"
 import containerStyles from "./PhotoContainer.module.css"
+// import * as Filter from "../filter/filter"
 
-export default class PhotoContainer {
-  componentDidMount() {
-
-  }
-
-  render() {
-    return (
-      <div className={containerStyles.container}>
-        <img
-          src="https://source.unsplash.com/random/400x200"
-          alt="random photo"
-        />
-      </div>
-    );
-  }
+export default function PhotoContainer() {
+  return (
+    <div className={containerStyles.container}>
+      <img
+        className='filter-image'
+        src="https://source.unsplash.com/random/400x200"
+        alt="random"
+      />
+    </div>
+  );
 }
