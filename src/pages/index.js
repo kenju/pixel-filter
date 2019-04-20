3f75b353f490de858c7d645ffe5ef229307b5e81
kenju <ken901waga@gmail.com>
2019-04-20 15:43:42 +0900

    remove header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import React from "react"
-import Header from "../components/header"
 import Layout from "../components/layout"
 
 export default () => (
   <Layout>
     <div style={{ color: `purple` }}>
-      <Header headerText="Hello Gatsby!" />
+      <h1>Hello Gatsby!</h1>
       <p>What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
