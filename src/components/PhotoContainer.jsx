d29feb3cecf9e346476aaf6a55897774dfc25b8b
kenju <ken901waga@gmail.com>
2019-04-20 16:09:32 +0900

    use class container

--- src/components/PhotoContainer.jsx
+++ src/components/PhotoContainer.jsx
@@ -1,13 +1,19 @@
 import React from "react"
 import containerStyles from "./PhotoContainer.module.css"
 
-export default function PhotoContainer({}) {
-  return (
-    <div className={containerStyles.container}>
-      <img
-        src="https://source.unsplash.com/random/400x200"
-        alt="random photo"
-      />
-    </div>
-  );
+export default class PhotoContainer {
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div className={containerStyles.container}>
+        <img
+          src="https://source.unsplash.com/random/400x200"
+          alt="random photo"
+        />
+      </div>
+    );
+  }
 }
