65e06d7959a9c289a05df7c435004e62ebf09ee2
kenju <ken901waga@gmail.com>
2019-04-20 16:27:10 +0900

    wip

--- src/components/PhotoContainer.jsx
+++ src/components/PhotoContainer.jsx
@@ -1,14 +1,22 @@
-import React from "react"
+import React, { useEffect } from "react"
 import containerStyles from "./PhotoContainer.module.css"
-// import * as Filter from "../filter/filter"
+import Filter from "../filter/filter"
+
+const filter = new Filter();
 
 export default function PhotoContainer() {
+  function handleOnLoad({ target }) {
+    console.log(target)
+    filter.process(target);
+  }
+
   return (
     <div className={containerStyles.container}>
       <img
         className='filter-image'
         src="https://source.unsplash.com/random/400x200"
         alt="random"
+        onLoad={handleOnLoad}
       />
     </div>
   );
---
+++ src/filter/filter.js
@@ -0,0 +1,50 @@
+export default class Filter {
+  constructor() {
+    this.canvas = {}
+  }
+
+  process(img) {
+    let pixels;
+    let worker;
+    let obj;
+    let effect = img.dataset.effect;
+
+    console.log('img=')
+    console.log(img)
+
+    pixels = this.getPixels(img);
+    worker = new Worker('./worker.js');
+    obj = {
+        pixels: pixels,
+        effects: effect
+    };
+    worker.postMessage(obj);
+    worker.onmessage = function (e) {
+        this.renderCanvas(img, e.data.pixels);
+    };
+  }
+
+  getPixels = function(img) {
+    let canvas
+    let context
+    canvas = this.getCanvas(img.width, img.height);
+    context = canvas.getContext('2d');
+    context.drawImage(img, 0, 0);
+    return context.getImageData(0, 0, canvas.width, canvas.height);
+  };
+
+  getCanvas = function(width, height) {
+    let canvas = document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
+    return canvas;
+  };
+
+  renderCanvas = function(img, new_pixels){
+    let canvas, context;
+    canvas  = this.getCanvas(img.width, img.height);
+    context = canvas.getContext("2d");
+    context.putImageData(new_pixels, 0, 0);
+    img.src = canvas.toDataURL();
+  };
+}
