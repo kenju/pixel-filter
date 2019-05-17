6dca689e6e5bccd38033cfa01de6d8b0a16bfb81
kenju <ken901waga@gmail.com>
2019-05-17 21:26:23 +0900

    add emoji favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: "standalone",
-        icon: "src/images/icon.jpeg", // This path is relative to the root of the site.
+        icon: "src/images/sample.jpeg", // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,9 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 import { Link, StaticQuery, graphql } from "gatsby"
+import favicon16 from "../favicons/favicon-16x16.png";
+import favicon32 from "../favicons/favicon-32x32.png";
+import favicon from "../favicons/favicon.ico";
 
 const ListLink = props => (
   <li style={{ display: `inline-block`, marginRight: `1rem` }}>
@@ -27,6 +30,9 @@ export default ({
           <meta charSet="utf-8" />
           <title>{data.site.siteMetadata.title}</title>
           <link rel="canonical" href="https://kenju.github.io/pixel-filter" />
+          <link rel="icon" type="image/png" sizes="16x16" href={favicon16} />
+          <link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
+          <link rel="shortcut icon" type="image/png" href={favicon} />
         </Helmet>
 
         <header style={{ marginBottom: `1.5rem` }}>
